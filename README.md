75dc65e2fb6452d8efa3903ee56004391f0da214
Ajayeswar Reddy <ajayeswarb2a@gmail.com>
2023-07-12 11:28:08 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
-# ResumeMatch
-End-to-end distributed deep learning pipeline to match match a resume with relevant jobs from the job postings.
+# ResumeMatcher
 
-Flow:
-. Scraped and stored job postings from multiple sources to GCP (Google cloud), preprocessed data using PySpark on Databricks, and stored collections in MongoDB (ELT pipeline). Utilized Airflow for scheduled scraping.
-. Parsed resumes and job postings and generated word embeddings on this cleaned data using Setence transformer models (large and small) 
-. Peformed vector similarity search between resume and all job postings embeddings to find the best match and suggest accordingly.
-. Achieved perfect accuracy for the manually tested 30 resumes
-. Clustered job postings using pyspark clustering libraries to group similar jobs together
+Match a resume to relevant open job postings
+
+Team: Prithvi Nuthanakalva, Bharadwaj Allu, Ajayeswar Peddyreddy, Tejaswi Samrat Dasari, Harrison Yu, Akhil Gopi
+
+#### Product Proposal
+
+Build a resume matching platform (i.e. when user uploads a resume, the system parses the resume and finds the most relevant job posting from the available job postings on the internet) - JobsForYou.
+
+
+### User Flow
+- User opens the "Jobs For You" application and is presented with a landing page as shown below.
+
+![landing page](./assets/landing.jpeg?raw=true "Landing page")
+
+- The user is supposed to upload a resume and click the submit button in the UI. Upon doing so, the system will recommend potential jobs that the user is a good match for, as shown below.
+
+![outputs](./assets/results.jpeg?raw=true "Results")
+
+### Steps Involved
+
+#### Data Collection
+- Periodically (using Airflow) use job listing APIs to retrieve the open job listings, and save the content to google cloud storage(GCS).
+  Eg APIs: https://rapidapi.com/letscrape-6bRBa3QguO5/api/jsearch to extract diverse job descriptions.
+- Aggregated the saved data from GCS and created a collection in Mongo Atlas.
+
+#### Encoding Job descriptions and Resumes
+
+- Sentence transformers are deep learning models that take in a sentence as input and generate a dense vector representation of that sentence as output. These vectors are designed to capture the semantic meaning of the input sentence, and therefore they can be used to compare and match sentences based on their meaning.
+
+- In the context of job descriptions, we can use sentence transformers to convert the text of job descriptions into numerical vectors. These vectors can then be used to match job descriptions with candidate resumes, or to cluster similar job descriptions together.
+
+#### Storing and retrieval using Pinecone
+
+- Once the job descriptions have been encoded into vectors using the sentence transformer model, we need to store these vectors in a way that allows us to efficiently search and retrieve them. We have used Pinecone to get this task done efficiently.
+
+- Pinecone is a cloud-based vector database that allows us to store and search high-dimensional vectors efficiently. By storing the encoded job descriptions in Pinecone, we can easily search for and retrieve the most relevant job descriptions based on a candidate's resume or search query.
+
+#### UI development
+- The task involves building a front-end application using Streamlit, a Python library used for creating web applications. The purpose of the front-end application is to allow users to upload a resume file in PDF format.In this case, the application is focused on allowing users to upload a PDF file of their resume.
+
+- Once the user has uploaded their resume file in pdf, it gets stored on the device, the path is sent as the argument to the Affinda API to generate
+text and further vectorize it to get the relevant job descriptions.
---
+++ main_app.py
@@ -0,0 +1,145 @@
+import streamlit as st
+import PyPDF2 
+from PIL import Image
+from user_definition import *
+from MongoClient import *
+import requests
+import os
+
+def main():
+    mon = MongoDBCollection(mongo_username, 
+                                mongo_password,
+                                mongo_ip_address,
+                                database_name,
+                                collection_name)
+    st.set_page_config(page_title='JobsForYou', page_icon=':sunglasses:', layout='wide', initial_sidebar_state='auto')
+    logo = Image.open('logo.png')
+    width, height = logo.size
+    # logo = logo.resize((width//2, height//2))
+    # st.image(logo) #,use_column_width='always')
+    text_style = """
+    <style>
+    .text {
+        /*background-color: black;*/
+        color: white;
+        padding: 0.5rem;
+        font-size: 3rem;
+        font-weight: bold;
+        border-radius: 5px;
+        text-align: center;
+    }
+
+    .tagline {
+        margin-top: 0.5rem;
+        font-size: 1.5rem;
+        font-weight: bold;
+        text-align: center;
+        color: #0072B5;
+    }
+    </style>
+    """
+
+    # Display the website name with the defined style
+    st.markdown(f'<div class="text">JobsForYou</div>', unsafe_allow_html=True)
+    # Display the tagline with the defined style
+    st.markdown(f'<div class="tagline">"Navigate Your Way to the Best Job with AI-Powered Resume Matching"</div>', unsafe_allow_html=True)
+    st.markdown(text_style, unsafe_allow_html=True)
+    instructions_placeholder = st.empty()
+    instructions_placeholder.subheader(f'''How to use JobsForYou:
+    1. Upload your resume in PDF format.
+    2. Submit your resume by clicking on the “Submit” button.
+    3. View the recommended jobs on the screen.''')
+    uploaded_file = st.file_uploader("Upload your resume in PDF format:", type=["pdf"])
+    # If file is uploaded, print absolute file path
+    if not os.path.exists('uploaded_resumes'):
+        os.makedirs('uploaded_resumes')
+    if uploaded_file:
+        instructions_placeholder.empty()
+        # st.header("Job Listings")
+        file_path = os.path.abspath('uploaded_resumes/'+uploaded_file.name)
+        with open(file_path,'wb') as f:
+            f.write(uploaded_file.read())
+        st.success(f"File uploaded: {uploaded_file.name}")
+        #call api
+        with st.spinner(text="In progress..."):
+            find_closest_match_url=f"{api_url}/find_closest_match"
+            recom_job_ids = requests.post(find_closest_match_url, json={'path':file_path}).json()
+            recom_job_objids = [ObjectId(job_id['id']) for job_id in recom_job_ids]
+            st.header("Recommended Job Listings")
+            recom_jobs=list(mon.find({'_id':{'$in':recom_job_objids}},{'_id':False}))
+        show_jobs(recom_jobs)
+        
+        # for job_description in jds:
+        #     st.write(f"## Job Description")
+        #     st.write(f"{job_description}")
+        #     st.write("---")
+            
+    # elif query:
+    #     find_query_url=f"{api_url}/search" 
+    #     recom_job_ids = requests.post(find_query_url, data=query).json()
+    #     recom_job_objids = [ObjectId(job_id) for job_id in recom_job_ids['job_ids']]
+    #     st.header("Search Results")
+    #     recom_jobs=[next(mon.find({'_id':oid},{'_id':False})) for oid in recom_job_objids]
+    #     show_jobs(recom_jobs)
+    
+
+        
+    # Get 100 job listings
+    #     n=100
+    #     jobs = mon.find_n(n)
+
+    #     # Show job listings
+    #     show_jobs(list(jobs))
+    
+
+def show_jobs(jobs):
+    # Show 100 jobs per page
+    num_jobs = len(jobs)
+    num_pages = int(num_jobs / 100) + 1
+
+    # Create container for job listings
+    container = st.container()
+
+    start_index = 0
+    end_index = min(start_index + 100, num_jobs)
+    col1, col2 = st.columns(2)
+    # Show job listings in vertical slider
+    with container:
+        job_blocks = []
+        # st.write('<style>div.row-widget.stHorizontal{flex-wrap: nowrap !important;}</style>', unsafe_allow_html=True)
+        # st.write('<style>.main .block-container {flex: 1 1 0px}</style>', unsafe_allow_html=True)
+        # st.write('<style>.block-container {max-height:500px;overflow:auto;} </style>',unsafe_allow_html=True)
+        for i in range(start_index, end_index):
+            if i%2==0:
+                with col1:
+                    job_title = jobs[i]["job_title"]
+                    employer = jobs[i]["employer_name"]
+                    job_description = jobs[i]["job_description"]
+                    job_block = st.expander(f":blue[{job_title}] - {employer}")
+                    job_block.write(job_description)
+                    job_blocks.append(job_block)
+            else:
+                with col2:
+                    job_title = jobs[i]["job_title"]
+                    employer = jobs[i]["employer_name"]
+                    job_description = jobs[i]["job_description"]
+                    job_block = st.expander(f":blue[{job_title}] - {employer}")
+                    job_block.write(job_description)
+                    job_blocks.append(job_block)
+        if end_index < num_jobs:
+            if st.button("Load more"):
+                start_index = end_index
+                end_index = min(start_index + 100, num_jobs)
+                for i in range(start_index, end_index):
+                    job_title = jobs[i]["job_title"]
+                    employer = jobs[i]["employer_name"]
+                    job_description = jobs[i]["job_description"][:100]+'...'
+                    job_block = st.expander(f":blue[{job_title}] - {employer}")
+                    job_block.write(job_description)
+                    job_blocks.append(job_block)
+
+    # Add slider for selecting page
+    # page_number = st.slider("Page", 1, num_pages, 1)
+
+if __name__ == "__main__":
+    main()
