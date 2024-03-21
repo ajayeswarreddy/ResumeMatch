f6855cd1c84e9e11138aa528d0eda0897f65004d
Ajayeswar Reddy <ajayeswarb2a@gmail.com>
2024-03-21 14:37:26 -0500

    Update user_definition.py

--- user_definition.py
+++ user_definition.py
@@ -1,9 +1,9 @@
 api_url = "http://127.0.0.1:5000"
 
 
-mongo_username = "mongoadmin"
-mongo_password = "pDru4hbGjnIGRCmm"
-mongo_ip_address = "cluster0.inorscz.mongodb.net"
-database_name = "msds697"
-collection_name = "job_postings"
+mongo_username = ""
+mongo_password = ""
+mongo_ip_address = ""
+database_name = ""
+collection_name = ""
 
